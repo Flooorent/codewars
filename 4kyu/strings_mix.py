f24e1ab9eb0b11b180f7b1efc6ad0c518ee25519
Florent Moiny <florent.moiny@gmail.com>
2016-08-30 23:11:40 +0200

    4kyu: added Unnamed's solution to strings mix

--- 4kyu/strings_mix.py
+++ 4kyu/strings_mix.py
@@ -34,3 +34,19 @@ def mix(s1, s2):
     res2 = map(lambda t: str(t[1]) + ':' + t[0]*t[2], res1)
     return '/'.join(res2)
 
+# Unnamed's solution, which basically obliterates mine
+
+from collections import Counter
+
+def mix(s1, s2):
+    c1 = Counter(filter(str.islower, s1))
+    c2 = Counter(filter(str.islower, s2))
+    res = []
+    for c in set(c1.keys() + c2.keys()):
+        n1, n2 = c1.get(c, 0), c2.get(c, 0)
+        if n1 > 1 or n2 > 1:
+            res.append(('1', c, n1) if n1 > n2 else
+                ('2', c, n2) if n2 > n1 else ('=', c, n1))
+    res = ['{}:{}'.format(i, c * n) for i, c, n in res]
+    return '/'.join(sorted(res, key=lambda s: (-len(s), s)))
+    
